ee74e61e5c800b27b6501273dfc10fab1ae4013e
nomayen <nomayen.ohin@gmail.com>
2022-08-23 01:16:20 +0600

    function added

--- js/function.js
+++ js/function.js
@@ -1,1 +1,21 @@
 
+// get input value
+function getInputVal (id) {
+    const str_val = document.getElementById(id).value
+    const val = parseInt(str_val)
+
+    
+    if (isNaN(val)) {
+        alert('Please provide Correct data')
+    }else if ( val < 0 ){
+        alert('Please provide Correct Positive number')
+    }
+     else {
+        return val
+    }
+
+}
+// comma seperated number
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
